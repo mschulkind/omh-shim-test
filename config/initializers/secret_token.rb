c3fd25e6c81d8f80ba97bedc0c89969ee6b12218
Matthew Schulkind <mschulkind@gmail.com>
2013-10-19 19:28:27 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitbitTest::Application.config.secret_token = 'df6db528409df664cec5d063654f31fcee56c07d336e2eac5f001658d041f4d3c2051426889bcff533de2ad71d62ab8f2725f319d500275b1c9f44a9e4e7b289'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FitbitTest::Application.config.session_store :cookie_store, key: '_fitbit-test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FitbitTest::Application.config.session_store :active_record_store
